684b942d8539498f53114fd52b76b52e9f819ef7
Prof Privitera <giuseppe.privitera@issgreppi.it>
2026-03-27 15:44:55 +0100

    colori sistemati dark e light

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
 			logo: {
         		src: './src/assets/greppi_net.png',
      				},
+			customCss: [
+        './src/styles/custom.css',
+      					],
 			sidebar: [
       					// --- PRIMA CATEGORIA ---
       					{
