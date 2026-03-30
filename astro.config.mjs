45d2504365831d8e10f49706498e2865e1180e98
Prof Privitera <giuseppe.privitera@issgreppi.it>
2026-03-30 10:53:23 +0200

    dispensa completa v1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,8 +18,9 @@ export default defineConfig({
 			sidebar: [
       					// --- PRIMA CATEGORIA ---
       					{
-        				label: 'Infrastrutture di Rete',
-        				items: [
+        				label: 'Infrastrutture di Rete',        				
+						collapsed: true,        				
+						items: [
           						{ label: 'Subnetting e VLSM', link: '/subnetting-vlsm/' },
          						{ label: 'Le VLAN e il Tagging', link: '/vlan/' },
 								{ label: 'Il routing', link: '/routing/' },
@@ -29,8 +30,9 @@ export default defineConfig({
 
      					// --- SECONDA CATEGORIA ---
       					{
-        				label: 'Sicurezza e Protezione',
-       					items: [
+        				label: 'Sicurezza e Protezione',        				
+						collapsed: true,       					
+						items: [
          						{ label: 'Le VPN', link: '/vpn/' },
 								{ label: 'MFA e Zero Trust', link: '/mfa-zero/' },
           						{ label: 'IDS, IPS e DPI', link: '/ids-ips-dpi/' },
@@ -42,8 +44,9 @@ export default defineConfig({
 
       					// --- TERZA CATEGORIA ---
       					{
-        				label: 'Tecnologie Avanzate',
-        				items: [
+        				label: 'Tecnologie Avanzate',        				
+						collapsed: true,        				
+						items: [
           						{ label: 'Le reti MPLS', link: '/mpls/' },
 								{ label: 'Le reti SD-WAN', link: '/sdwan/' },
           						{ label: 'IoT e MQTT', link: '/iot-mqtt/' },
@@ -53,6 +56,7 @@ export default defineConfig({
 						// quarta categoria
 						{
         				label: 'Servizi di Rete e Livello Applicativo',
+        				collapsed: true,
         				items: [
           						{ label: 'Il Protocollo DHCP', link: '/dhcp/' },
           						{ label: 'Il sistema DNS', link: '/dns/' },
@@ -62,6 +66,7 @@ export default defineConfig({
       					},
 						{
         				label: 'Cloud Computing e Virtualizzazione',
+        				collapsed: true,
         				items: [
           						{ label: 'Virtualizzazione e Hypervisor', link: '/virtualizzazione-hypervisor/' },
           						{ label: 'Containerizzazione', link: '/containerizzazione/' },
@@ -72,24 +77,28 @@ export default defineConfig({
 						// quinta categoria
 						{
         				label: 'Alta Affidabilità e Resilienza',
+        				collapsed: true,
         				items: [
 								{ label: 'Business continuity e Disaster Recovery', link: '/business-disaster/' }
         						]
       					},
 						{
         				label: 'Sviluppo Dati e Applicazioni',
+        				collapsed: true,
         				items: [
 								{ label: 'Progettazione DB e Sviluppo web/API REST', link: '/info/' }
         						]
       					},
 						{
         				label: 'Cablaggio Strutturato',
+        				collapsed: true,
         				items: [
 								{ label: 'Introduzione al Cablaggio Strutturato', link: '/cab-strutt/' }
         						]
       					},
 						{
         				label: 'Seconda Prova',
+        				collapsed: true,
         				items: [
 								{ label: 'Struttura sviluppo traccia', link: '/sviluppo-traccia/' },
 								{ label: 'Testo traccia', link: '/testo-traccia/' },
@@ -98,6 +107,7 @@ export default defineConfig({
       					},
 						{
         				label: 'Educazione Civica',
+        				collapsed: true,
         				items: [
           						{ label: 'Confini reali e confini virtuali', link: '/civica/' }
         						]
